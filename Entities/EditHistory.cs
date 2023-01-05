efe2d9266dcddeaa1745ac7c719d7a2032a2ffaf
Elijah Aleakhue <ealeakhue@axxess.com>
2023-01-05 19:00:51 +0100

    first commit

---
+++ Entities/EditHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class EditHistory
+    {
+        public int Id { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ContactId { get; set; }
+    }
+}
